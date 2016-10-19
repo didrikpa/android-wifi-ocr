ae98ee786ba2562b1abbd201b5176b7dc9f7ff00
didrikpa <didrikpa@gmail.com>
2016-10-19 22:07:55 +0200

    Initial commit

---
+++ app/src/main/java/com/development/didrikpa/wifipasswordscanner/MainActivity.java
@@ -0,0 +1,110 @@
+package com.development.didrikpa.wifipasswordscanner;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+
+    WifiManager mainWifi;
+    WifiReceiver receiverWifi;
+
+    StringBuilder sb = new StringBuilder();
+
+    private final Handler handler = new Handler();
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        receiverWifi = new WifiReceiver();
+        registerReceiver(receiverWifi, new IntentFilter(
+                WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        if (mainWifi.isWifiEnabled() == false) {
+            mainWifi.setWifiEnabled(true);
+        }
+
+
+        doInback();
+    }
+
+    public void doInback() {
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+                receiverWifi = new WifiReceiver();
+                registerReceiver(receiverWifi, new IntentFilter(
+                        WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+                mainWifi.startScan();
+                doInback();
+            }
+        }, 1000);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 0, 0, "Refresh");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        mainWifi.startScan();
+
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(receiverWifi);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        registerReceiver(receiverWifi, new IntentFilter(
+                WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
+    }
+
+    class WifiReceiver extends BroadcastReceiver {
+        public void onReceive(Context c, Intent intent) {
+
+            ArrayList<String> connections = new ArrayList<String>();
+            ArrayList<Float> Signal_Strenth = new ArrayList<Float>();
+
+            sb = new StringBuilder();
+            List<ScanResult> wifiList;
+            wifiList = mainWifi.getScanResults();
+            for (int i = 0; i < wifiList.size(); i++) {
+
+                connections.add(wifiList.get(i).SSID);
+            }
+
+
+        }
+    }
+
+
+}
